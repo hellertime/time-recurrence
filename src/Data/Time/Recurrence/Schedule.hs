ed4dbe6e301ba94049aeb2a7d07476f1a366991e
Chris Heller <hellertime@gmail.com>
2011-06-04 21:10:00 -0400

    Right now I'm getting the functions right.
    Next. Getting the types right.

--- src/Data/Time/Recurrence/Schedule.hs
+++ src/Data/Time/Recurrence/Schedule.hs
@@ -14,13 +14,16 @@ module Data.Time.Recurrence.Schedule
     , only
     , byWeekDays
 -}
+    , enumDays
+    , enumWeekDays
+    , enumMonths
     )
   where
 
 import Control.Monad.Reader
 import Data.List as L
 import Data.List.Ordered as O
-import Data.Maybe (fromJust, fromMaybe)
+import Data.Maybe (mapMaybe)
 import Data.Time.Calendar.Month
 import Data.Time.Calendar.WeekDay
 import Data.Time.CalendarTime hiding (withDay)
@@ -59,7 +62,54 @@ starting im m0 = dropWhile (< m0) . O.nub . repeatSchedule im{moment=m0}
 begin :: (Ord a, Moment a) => InitialMoment a -> a -> [a]
 begin im m0 = starting im m0 (return)
 
+-- | Normalize an bounded ordinal index
+--   Pass an upper-bound 'ub' and an index 'idx'
+--   Converts 'idx' < 0 into valid 'idx' > 0 or
+--   Nothing
+normalizeOrdinalIndex :: Int -> Int -> Maybe Int
+normalizeOrdinalIndex _ 0 = Nothing
+normalizeOrdinalIndex ub idx =
+  if abs idx > ub
+    then Nothing
+    else Just $ (idx + ub') `mod` ub'
+  where
+    ub' = ub + 1
 
+enumMonths :: 
+  (CalendarTimeConvertible a, Moment a) => 
+  [Month] 
+  -> [a] 
+  -> Schedule a
+enumMonths months as = return $ concatMap (enumMonths' months) as
+  where 
+    enumMonths' months a = mapMaybe (withMonth a) months
+
+enumDays ::
+  (CalendarTimeConvertible a, Moment a) =>
+  [Int]
+  -> [a]
+  -> Schedule a
+enumDays days as = return $ concatMap (enumDays' days) as
+  where 
+    enumDays' days a = mapMaybe (withDay a) (days' a days)
+    days' a = mapMaybe $ normalizeOrdinalIndex (lastDayOfMonth a)
+
+enumWeekDays ::
+  (CalendarTimeConvertible a, Moment a) =>
+  [WeekDay]
+  -> [a]
+  -> Schedule a
+enumWeekDays wdays as = return $ concatMap (enumWeekDays' wdays) as
+  where
+    enumWeekDays' :: (CalendarTimeConvertible a, Moment a) => [WeekDay] -> a -> [a]
+    enumWeekDays' wdays a0 = let
+      m0 = calendarMonth $ toCalendarTime a0
+      in flip unfoldr (a0,cycle wdays) (\(a, (w:ws)) -> let
+           a'       = advanceToWeekDay a w
+           in if m0 == (calendarMonth $ toCalendarTime a)
+                then Just (a, (a', tail ws))
+                else Nothing
+          )
 
 {-
 -- | 'onMonths' computes the bounds for the 'month' given a 'moment', and
